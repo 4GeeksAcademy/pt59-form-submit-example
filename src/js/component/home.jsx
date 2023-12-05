93f4315c3e7d46d2c057675186b3cbe64d9f3f0f
Shane B <dotfortun@users.noreply.github.com>
2023-12-05 16:36:38 +0000

    Minimal form submit example

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="container">
+      <form
+        className="d-flex flex-column gap-3 "
+        onSubmit={(e) => {
+          // e.preventDefault() prevents the page from
+          // reloading when you submit the form.
+          e.preventDefault();
+          // This event triggers when you submit the form,
+          // so all your logic for that should live here.
+          console.log(value);
+          setValue("");
+        }}
+      >
+        <h1>Open the console to see this in action!</h1>
+        {/* This is a standard controlled input. */}
+        {/* Read more here: https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable */}
+        <input
+          placeholder="Type here and hit enter!"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="btn btn-primary">
+          This button also works with <code>0</code> lines of code!
+        </button>
+        <h2>
+          Check out <code>/src/js/component/home.jsx</code> to see how this
+          works!
+        </h2>
+        <h2>
+          Read about controlled inputs{" "}
+          <a
+            href="https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable"
+            target="_blank"
+          >
+            here!
+          </a>
+        </h2>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
